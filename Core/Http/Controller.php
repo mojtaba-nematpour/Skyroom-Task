27ed1fc41fd4bb051df15ea793430b58e663ab7d
Mojey Nmtpr <mjtba.nmtpr@gmail.com>
2024-06-01 15:00:17 +0330

    Add connection in controllers

--- Core/Http/Controller.php
+++ Core/Http/Controller.php
@@ -2,17 +2,31 @@
 
 namespace Core\Http;
 
+use Core\Database\Connection;
 use Core\Http\Responses\JsonResponse;
 
 class Controller
 {
     protected ?Request $request = null;
 
-    public function setRequest(Request $request): void
+    protected ?Connection $connection = null;
+
+    public function setRequest(Request $request): static
     {
         if ($this->request === null) {
             $this->request = $request;
         }
+
+        return $this;
+    }
+
+    public function setModelManager(?Connection $con): static
+    {
+        if ($this->connection === null) {
+            $this->connection = $con;
+        }
+
+        return $this;
     }
 
     protected function json(mixed $data, int $status = 200): JsonResponse
