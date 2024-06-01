f98ca53acd4db184c20ae1e2179750a8a2ab0467
Mojey Nmtpr <mjtba.nmtpr@gmail.com>
2024-06-01 14:01:59 +0330

    Change: add service injection for controllers

--- Core/Basic/Kernel.php
+++ Core/Basic/Kernel.php
@@ -6,6 +6,7 @@ use Core\Http\Controller;
 use Core\Http\Request;
 use Core\Http\Response;
 use Exception;
+use ReflectionMethod;
 
 class Kernel
 {
@@ -30,9 +31,21 @@ class Kernel
                 throw new Exception("$controller does not respond to the $method action.");
             }
 
+            $parameters = [];
+            $reflectedMethod = new ReflectionMethod($controller, $method);
+            $services = $reflectedMethod->getParameters();
+            foreach ($services as $service) {
+                $serviceName = $service->getType()->getName();
+                if (!class_exists($serviceName)) {
+                    continue;
+                }
+
+                $parameters[] = new $serviceName();
+            }
+
             $controller->setRequest($request);
 
-            return $controller->$method();
+            return $controller->$method(...$parameters);
         } catch (Exception $e) {
             $response = new Response($e->getMessage(), 404);
         }
