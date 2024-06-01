4102177a9dba7c54d21252d987bfb09a19468bba
Mojey Nmtpr <mjtba.nmtpr@gmail.com>
2024-06-01 14:01:07 +0330

    Change: add methods for removing and getting parameters of request

--- Core/Http/Request.php
+++ Core/Http/Request.php
@@ -41,9 +41,37 @@ class Request
         return $name === null ? $this->attributes : $this->attributes[$name] ?? null;
     }
 
+    public function setAttr(string $name, mixed $data): static
+    {
+        $this->attributes[$name] = $data;
+
+        return $this;
+    }
+
+    public function removeAttr(string $name): static
+    {
+        unset($this->attributes[$name]);
+
+        return $this;
+    }
+
     public function queries(string $name = null): mixed
     {
         return $name === null ? $this->queries : $this->queries[$name] ?? null;
+    }
+
+    public function setQuery(string $name, mixed $data): static
+    {
+        $this->queries[$name] = $data;
+
+        return $this;
+    }
+
+    public function removeQuery(string $name): static
+    {
+        unset($this->queries[$name]);
+
+        return $this;
     }
 
     public function requests(string $name = null): mixed
@@ -51,8 +79,36 @@ class Request
         return $name === null ? $this->requests : $this->requests[$name] ?? null;
     }
 
+    public function setRequest(string $name, mixed $data): static
+    {
+        $this->requests[$name] = $data;
+
+        return $this;
+    }
+
+    public function removeRequest(string $name): static
+    {
+        unset($this->requests[$name]);
+
+        return $this;
+    }
+
     public function files(string $name = null): mixed
     {
         return $name === null ? $this->files : $this->files[$name] ?? null;
+    }
+
+    public function setFile(string $name, mixed $data): static
+    {
+        $this->files[$name] = $data;
+
+        return $this;
+    }
+
+    public function removeFile(string $name): static
+    {
+        unset($this->files[$name]);
+
+        return $this;
     }
 }
